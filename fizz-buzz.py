ed556daaff8ef26f20c7ba1444be112143643093
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-02 15:11:03 -0700

    updated fizzbuzz solution 1

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -22,7 +22,7 @@
 # step 6: else, return number
 
 def fizzBuzz(n):
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
